c1dd2e7f37256fab5c200c6db18728020d2ecbef
osuminer <connorstaschiak@gmail.com>
2024-01-02 22:37:04 -0500

    added file upload implementation to AddBottle

--- src/pages/AddBottle.jsx
+++ src/pages/AddBottle.jsx
@@ -27,6 +27,8 @@ const AddBottle = () => {
   const [image, setImage] = useState("");
   const [validated, setValidated] = useState(false);
 
+  const [selectedFile, setSelectedFile] = useState(null);
+
   // Form useState's
   const [name, setName] = useState("");
   const [type, setType] = useState("");
@@ -84,7 +86,8 @@ const AddBottle = () => {
       e.stopPropagation();
     } else {
       e.preventDefault();
-      console.log(whiskyData);
+      console.log(whiskyData)
+      
       try {
         const response = await fetch("https://api.cstasnet.com/api/whiskies", {
           method: "POST",
@@ -115,6 +118,40 @@ const AddBottle = () => {
       setValidated(true);
     }
   };
+
+  const handleFileChange = (event) => {
+    const file = event.target.files[0];
+    setSelectedFile(file);
+  };
+
+  const handleUpload = async () => {
+    if (!selectedFile) {
+      console.error("No file selected");
+      return;
+    }
+
+    try {
+      const formData = new FormData();
+      formData.append("file", selectedFile);
+
+      const response = await fetch("https://cdn.cstasnet.com/", {
+        method: "POST",
+        body: formData,
+      });
+
+      if (response.ok) {
+        const result = await response.json();
+        console.log("File uploaded successfully:", result.filename);
+        setUrl(`https://cdn.cstasnet.com/${result.filename}`)
+        setImage(`https://cdn.cstasnet.com/${result.filename}`)
+      } else {
+        console.error("Failed to upload file");
+      }
+    } catch (error) {
+      console.error("Error uploading file:", error);
+    }
+  };
+
 
   return (
     <Container>
@@ -215,6 +252,7 @@ const AddBottle = () => {
         <hr />
 
         <Row className="mt-3">
+          {/* URL Box */}
           <Col className="col-12 col-md-5">
             <InputGroup>
               <Form.Control
@@ -223,17 +261,31 @@ const AddBottle = () => {
                 onChange={(e) => setUrl(e.target.value)}
                 onKeyDown={handleKeyDown}
               ></Form.Control>
-
-              <Button className="px-4" onClick={() => handleSearch(image)}>
+              <Button className="px-4" variant="dark" onClick={() => handleSearch(image)}>
                 <i className="bi bi-search"></i>
               </Button>
             </InputGroup>
           </Col>
-          <Col className="col-12 col-md-2 my-3 my-md-0" style={{ textAlign: "center" }}>
+
+          {/* OR */}
+          <Col
+            className="col-12 col-md-2 my-3 my-md-0"
+            style={{ textAlign: "center" }}
+          >
             Or
           </Col>
+
+          {/* Upload Image Box */}
           <Col className="col-12 col-md-5">
-            <Form.Control type="file"></Form.Control>
+            <InputGroup>
+              <Form.Control type="file" onChange={handleFileChange}></Form.Control>
+              <Button variant="dark" onClick={handleUpload}>
+                <span>
+                  <i className="bi bi-upload" style={{paddingRight: "10px"}}></i>
+                  Upload
+                </span>
+              </Button>
+            </InputGroup>
           </Col>
         </Row>
 
